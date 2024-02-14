a28b8ac5df84f4fb7f7e47f6f7123f1eb525e931
JAldo1980 <jamesalderman1980@gmail.com>
2024-02-14 10:21:33 +0000

    added JSX components

---
+++ react-todo-list-v1/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Container from "./components/Container";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Container>
+        <h1>TEST</h1>
+      </Container>
+    </>
+  );
+}
+
+export default App;
